bdf096b77c71b86455464d454261bc4ecba0d766
Christopher Stoll <stollcri@gmail.com>
2014-12-25 22:33:09 -0500

    Load defaults from the settings plist

--- Squared/AppDelegate.m
+++ Squared/AppDelegate.m
@@ -18,8 +18,14 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
+    /*
     NSDictionary *appDefaults = [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects:[NSNumber numberWithBool:YES], @5, nil] forKeys:[NSArray arrayWithObjects:@"padRectangularImages", @"cutsPerItteration", nil]];
     [[NSUserDefaults standardUserDefaults] registerDefaults:appDefaults];
+    */
+    NSURL *settingsBundleURL = [[NSBundle mainBundle] URLForResource:@"Settings" withExtension:@"bundle"];
+    NSDictionary *appDefaults = [self loadDefaultsFromSettingsPage:@"Root.plist" inSettingsBundleAtURL:settingsBundleURL];
+    [[NSUserDefaults standardUserDefaults] registerDefaults:appDefaults];
+    [[NSUserDefaults standardUserDefaults] synchronize];
     
     return YES;
 }
@@ -44,6 +50,69 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+//| ----------------------------------------------------------------------------
+//! Helper function that parses a Settings page file, extracts each preference
+//! defined within along with its default value.  If the page contains a
+//! 'Child Pane Element', this method will recurs on the referenced page file.
+//
+- (NSDictionary*)loadDefaultsFromSettingsPage:(NSString*)plistName inSettingsBundleAtURL:(NSURL*)settingsBundleURL
+{
+    // Each page of settings is represented by a property-list file that follows
+    // the Settings Application Schema:
+    // <https://developer.apple.com/library/ios/#documentation/PreferenceSettings/Conceptual/SettingsApplicationSchemaReference/Introduction/Introduction.html>.
+    
+    // Create an NSDictionary from the plist file.
+    NSDictionary *settingsDict = [NSDictionary dictionaryWithContentsOfURL:[settingsBundleURL URLByAppendingPathComponent:plistName]];
+    
+    // The elements defined in a settings page are contained within an array
+    // that is associated with the root-level PreferenceSpecifiers key.
+    NSArray *prefSpecifierArray = settingsDict[@"PreferenceSpecifiers"];
+    
+    // If prefSpecifierArray is nil, something wen't wrong.  Either the
+    // specified plist does ot exist or is malformed.
+    if (prefSpecifierArray == nil)
+        return nil;
+    
+    // Create a dictionary to hold the parsed results.
+    NSMutableDictionary *keyValuePairs = [NSMutableDictionary dictionary];
+    
+    for (NSDictionary *prefItem in prefSpecifierArray)
+        // Each element is itself a dictionary.
+    {
+        // What kind of control is used to represent the preference element in the
+        // Settings app.
+        NSString *prefItemType = prefItem[@"Type"];
+        // How this preference element maps to the defaults database for the app.
+        NSString *prefItemKey = prefItem[@"Key"];
+        // The default value for the preference key.
+        NSString *prefItemDefaultValue = prefItem[@"DefaultValue"];
+        
+        if ([prefItemType isEqualToString:@"PSChildPaneSpecifier"])
+            // If this is a 'Child Pane Element'.  That is, a reference to another
+            // page.
+        {
+            // There must be a value associated with the 'File' key in this preference
+            // element's dictionary.  Its value is the name of the plist file in the
+            // Settings bundle for the referenced page.
+            NSString *prefItemFile = prefItem[@"File"];
+            
+            // Recurs on the referenced page.
+            NSDictionary *childPageKeyValuePairs = [self loadDefaultsFromSettingsPage:prefItemFile inSettingsBundleAtURL:settingsBundleURL];
+            
+            // Add the results to our dictionary
+            [keyValuePairs addEntriesFromDictionary:childPageKeyValuePairs];
+        }
+        else if (prefItemKey != nil && prefItemDefaultValue != nil)
+            // Some elements, such as 'Group' or 'Text Field' elements do not contain
+            // a key and default value.  Skip those.
+        {
+            keyValuePairs[prefItemKey] = prefItemDefaultValue;
+        }
+    }
+    
+    return keyValuePairs;
 }
 
 @end
